a51e7a53fe4a68da20ddb6f9b5310b613a169f66
Himanshu Sharma <107118596+Himanshu8445@users.noreply.github.com>
2022-11-23 16:42:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,7 @@
-module "resourcegroup" {
-  source          = "https://github.com/Himanshu8445/common/tree/main/resourcegroup"
-  resource_groups = var.resource_groups
-}
-
-resource "azurerm_storage_account" "storage_acct" {
-  for_each   = var.storage_accounts
-  depends_on = [module.resourcegroup]
-  name       = each.value.name
-
-  resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
-  location                      = each.value.location != null ? each.value.location : var.location
-  account_tier                  = "Standard"
-  account_replication_type      = "LRS"
-  public_network_access_enabled = "false"
-  blob_properties {
-    delete_retention_policy {
-      days = 7
-    }
-    versioning_enabled = true
-  }
-
-  tags = var.sa_additional_tags
-
-}
-
-resource "azurerm_storage_container" "management" {
-  for_each              = var.containers
-  depends_on            = [azurerm_storage_account.storage_acct]
-  name                  = each.value.name
-  storage_account_name  = each.value.storage_account_name
-  container_access_type = each.value.container_access_type
+# Create Resource Groups and assign mandatory tags
+resource "azurerm_resource_group" "this" {
+  for_each = var.resource_groups
+  name     = each.value["name"]
+  location = each.value["location"]
+  tags     = each.value["tags"]
 }
