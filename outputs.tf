ea3800ed87e59cfa47ab395f12d701f4eb45e20e
Himanshu Sharma <107118596+Himanshu8445@users.noreply.github.com>
2022-11-23 16:43:36 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,18 @@
-# output "admin_rg" {
-#   value = azurerm_resource_group.admin_resource_group
-##}
+# #############################################################################
+# # OUTPUTS Resource Group
+# #############################################################################
 
-output "admin_storage_acount" {
-  value     = azurerm_storage_account.storage_acct
-  sensitive = true
+output "resource_group_ids_map" {
+  value       = { for r in azurerm_resource_group.this : r.name => r.id }
+  description = "The Map of the Resource Group Id's."
 }
 
-output "resourcegroup" {
-  value     = module.resourcegroup
-  sensitive = true
+output "resource_group_locations_map" {
+  value       = { for r in azurerm_resource_group.this : r.name => r.location }
+  description = "The Map of the Resource Group Locations's."
+}
+
+output "resource_group_tags_map" {
+  value       = { for r in azurerm_resource_group.this : r.name => r.tags }
+  description = "The Map of the Resource Group Tag's."
 }
